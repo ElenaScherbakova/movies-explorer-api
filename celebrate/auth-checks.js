f9411a3465be6e59b4543988d294db1cfa802c21
Elena Shcherbakova <25pochta25@mail.ru>
2023-04-30 16:01:49 +0200

    ошибки при регистрации

--- celebrate/auth-checks.js
+++ celebrate/auth-checks.js
@@ -4,9 +4,9 @@ module.exports = {
 
   signUpCheck: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      email: Joi.string().email(),
-      password: Joi.string(),
+      name: Joi.string().required().min(2).max(30),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
     }),
   }),
 
